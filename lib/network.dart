227762becab0b44f99324cf3ebc6c3beb51dcaac
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2020-08-06 20:09:08 -0300

    Secao 14 - Aula 177 - Desafio feito, mas com tres chamadas em vez de uma

--- lib/network.dart
+++ lib/network.dart
@@ -2,15 +2,20 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-const Map<String, String> kHeaders = {
-  'X-CoinAPI-Key': 'AEA854D0-4A28-4E1E-B127-7AA37F7FE460'
-};
-
-const kUrlBase = 'https://rest.coinapi.io/v1/exchangerate/BTC/USD';
-
 class Network {
-  Future<dynamic> fetchCoin() async {
-    http.Response response = await http.get(kUrlBase, headers: kHeaders);
+  final kUrlBase = 'https://rest.coinapi.io/v1/exchangerate';
+
+  final Map<String, String> kHeaders = {
+    'X-CoinAPI-Key': 'AEA854D0-4A28-4E1E-B127-7AA37F7FE460'
+  };
+
+//  https://rest.coinapi.io/v1/exchangerate/USD/ETH
+
+  Future<dynamic> fetchCoin(
+      String selectedCoin, String selectedCurrency) async {
+    print(kUrlBase);
+    http.Response response = await http
+        .get('$kUrlBase/$selectedCoin/$selectedCurrency', headers: kHeaders);
     if (response.statusCode == 200) {
       String data = response.body;
       return jsonDecode(data);
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -12,10 +12,14 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   String selectedCurrency = 'USD';
+  String valueSelectedCurrencyBTC = '?';
+  String valueSelectedCurrencyETH = '?';
+  String valueSelectedCurrencyLTC = '?';
 
   @override
   void initState() {
-    resultGet();
+    super.initState();
+    getValueCoinCurrency();
   }
 
   List<Text> getCupertinoPickerItems() {
@@ -34,6 +38,7 @@ class _PriceScreenState extends State<PriceScreen> {
       onSelectedItemChanged: (selectIndex) {
         setState(() {
           selectedCurrency = currenciesList[selectIndex];
+          getValueCoinCurrency();
         });
       },
     );
@@ -60,6 +65,7 @@ class _PriceScreenState extends State<PriceScreen> {
       onChanged: (value) {
         setState(() {
           selectedCurrency = value;
+          getValueCoinCurrency();
         });
       },
     );
@@ -75,29 +81,16 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.blue,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? $selectedCurrency',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildButton('BTC', valueSelectedCurrencyBTC, selectedCurrency),
+              buildButton('ETH', valueSelectedCurrencyETH, selectedCurrency),
+              buildButton('LTC', valueSelectedCurrencyLTC, selectedCurrency),
+            ],
           ),
           Container(
-            height: 150.0,
+            height: 80.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
@@ -110,8 +103,48 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 
-  Future<dynamic> resultGet() async {
-    var result = await Network().fetchCoin();
-    return print(result.body);
+  Padding buildButton(String coin, String value, String currency) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.blue,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 coin = $value $currency',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> getValueCoinCurrency() async {
+    valueSelectedCurrencyBTC = '?';
+    valueSelectedCurrencyETH = '?';
+    valueSelectedCurrencyLTC = '?';
+
+    var resultBTC = await Network().fetchCoin('BTC', selectedCurrency);
+    var resultETH = await Network().fetchCoin('ETH', selectedCurrency);
+    var resultLTC = await Network().fetchCoin('LTC', selectedCurrency);
+
+    setState(() {
+      double rateBTC = resultBTC['rate'];
+      valueSelectedCurrencyBTC = rateBTC.toStringAsFixed(2).toString();
+
+      double rateETH = resultETH['rate'];
+      valueSelectedCurrencyETH = rateETH.toStringAsFixed(2).toString();
+
+      double rateLTC = resultLTC['rate'];
+      valueSelectedCurrencyLTC = rateLTC.toStringAsFixed(2).toString();
+    });
   }
 }
